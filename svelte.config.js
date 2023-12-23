dfff1118ac2f7092d2369ec1000be6b082d40dfc
Trongar <sanchesedgar97@gmail.com>
2023-12-23 11:36:45 -0600

    adapter

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,11 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'dist',
+			assets: 'dist',
+			fallback: 'index.html',
+		}),
 		alias: {
 			'@config': 'src/config.ts',
 			'@components': 'src/lib/components/index.ts',
